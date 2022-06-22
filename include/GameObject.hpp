d0be1f2767ca99b00e6bf81a48abe8f6b1a46ff4
Ahmad <huntertom417@gmail.com>
2022-06-23 01:29:00 +0500

    added an update function and a velocity property

--- include/GameObject.hpp
+++ include/GameObject.hpp
@@ -2,17 +2,24 @@
 #define GAME_OBJECT
 
 #include "SDL2/SDL.h"
+#include "../include/Vector2.hpp"
+
+//TODO: Implement Copy and Move constructors
+
 class GameObject{
 public:
-    GameObject(const char* sprite,SDL_Renderer* renderer, int collision_radius, const SDL_Rect& pos_size, double direction);
+    GameObject(const char* sprite,SDL_Renderer* renderer, int collision_radius, const SDL_FRect& pos_size, double direction);
     void draw(SDL_Renderer* renderer);
-
+    void update(float delta_time);
+    void set_pos(const Vector2 new_pos);
+   // bool collide(const GameObject& other);
     ~GameObject();
 
 private:
+    Vector2 velocity={1,1};
     int collision_radius; //radius for the collision circle
     double direction; //direction of the object
-    SDL_Rect pos_size; //position of object and size of object
+    SDL_FRect pos_size; //position of object and size of object
     SDL_Texture* sprite_texture;
 };
 
--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -2,17 +2,26 @@
 #include "SDL2/SDL_image.h"
 
 GameObject::GameObject(const char* sprite, SDL_Renderer* renderer,
-                    int collision_radius, const SDL_Rect& pos_size, double direction)
+                    int collision_radius, const SDL_FRect& pos_size, double direction)
                     :collision_radius(collision_radius), pos_size(pos_size),direction(direction) {
+
     SDL_Surface* sprite_surface = IMG_Load(sprite);
     sprite_texture = SDL_CreateTextureFromSurface(renderer, sprite_surface);
     SDL_FreeSurface(sprite_surface);
 }
  
 void GameObject::draw( SDL_Renderer* renderer){
-    SDL_RenderCopyEx(renderer,sprite_texture,NULL,&pos_size,direction,NULL, SDL_FLIP_NONE);
+    SDL_RenderCopyExF(renderer,sprite_texture,NULL,&pos_size,direction,NULL, SDL_FLIP_NONE);
 }
-
+void GameObject::update(float delta_time){
+    pos_size.x = pos_size.x + velocity.m_x*delta_time;
+    pos_size.y = pos_size.y + velocity.m_y*delta_time;
+}
 GameObject::~GameObject(){
     SDL_DestroyTexture(sprite_texture);
+}
+
+void GameObject::set_pos(const Vector2 new_pos){
+    pos_size.x = new_pos.m_x;
+    pos_size.y = new_pos.m_y;
 }
