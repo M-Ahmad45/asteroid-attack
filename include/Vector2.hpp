1c0ffec6079f9de55db2629c088155a43d2a23bd
Ahmad <huntertom417@gmail.com>
2022-06-24 02:01:57 +0500

    drop m_ prefix

--- include/Vector2.hpp
+++ include/Vector2.hpp
@@ -3,9 +3,9 @@
 
 class Vector2{
 public:
-    Vector2():m_x(0.0f),m_y(0.0f){}
-    Vector2(float x, float y):m_x(x), m_y(y){}
-    float m_x,m_y;
+    Vector2():x(0.0f),y(0.0f){}
+    Vector2(float x, float y):x(x), y(y){}
+    float x,y;
 };
 
 
